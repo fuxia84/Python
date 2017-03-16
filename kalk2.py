a58d0824b6ac05c6e730e92cc29877a61ad53ad6
fuxia84 <fuxia@wp.pl>
2017-03-16 19:59:31 +0100

    I'v added multiply

---
+++ kalk2.py
@@ -0,0 +1,14 @@
+import sys
+
+#print(sys.argv)
+
+def divide(a,b):
+    return a/b
+
+def main():
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    return divide(a,b)
+
+    if __name__ == __main__:
+        divide(a,b)
